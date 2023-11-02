39664b8e34c3bbbcd617d68bacac2d70f535d2d3
James Darnley <jdarnley@obe.tv>
2023-11-02 17:31:29 +0100

    Optimising SDI CRC: _mm_clmulepi64_si128

--- snippets.c
+++ snippets.c
@@ -1,52 +1,62 @@
-uint16_t table[1u << 10];
+uint64_t pack60(const uint16_t* data) {
+    return (((uint64_t)data[ 0]) <<  4) ^
+           (((uint64_t)data[ 2]) << 14) ^
+           (((uint64_t)data[ 4]) << 24) ^
+           (((uint64_t)data[ 6]) << 34) ^
+           (((uint64_t)data[ 8]) << 44) ^
+           (((uint64_t)data[10]) << 54);
+}
 
-void make_table() {
-    for (uint32_t idx = 0; idx < (1u << 10); idx++) {
-        uint32_t c = idx;
-        for (int k = 0; k < 10; k++) {
-            c = c & 1 ? (c >> 1) ^ 0x23000 : c >> 1;
-        }
-        table[idx] = (c >> 2);
-    }
+__m128i pack120(const uint16_t* data) {
+    return _mm_set_epi64x(pack60(data + 12) >> 4, pack60(data));
+}
+
+__m128i xor_clmul(__m128i a, __m128i b) {
+    return _mm_xor_si128(_mm_clmulepi64_si128(a, b, 0x00),
+                         _mm_clmulepi64_si128(a, b, 0x11));
 }
 
 void crc_sdi(uint32_t* crcs, const uint16_t* data, size_t n) {
-    uint32_t c = crcs[0];
-    uint32_t y = crcs[1];
-    for (size_t i = 0; i < n; i += 2) {
-        c ^= data[i];
-        y ^= data[i+1];
-        c = (table[c & 0x3ff] << 2) ^ (c >> 10);
-        y = (table[y & 0x3ff] << 2) ^ (y >> 10);
+    __m128i c = _mm_cvtsi32_si128(crcs[0]);
+    __m128i y = _mm_cvtsi32_si128(crcs[1]);
+    { // *= x^-14 semi-mod P
+        __m128i k = _mm_cvtsi32_si128(
+            0x9f380000 /* x^-14-(64-18)-32-1 mod P */);
+        c = _mm_clmulepi64_si128(c, k, 0x00);
+        y = _mm_clmulepi64_si128(y, k, 0x00);
     }
-    crcs[0] = c;
-    crcs[1] = y;
-}
-
-void crc_sdi(polynomial* crcs, const polynomial* data, size_t n) {
-    polynomial c = (crcs[0] * x-14) semi-mod P;
-    polynomial y = (crcs[1] * x-14) semi-mod P;
     for (size_t i = 0; i < n; i += 24) {
-        c = (c * x120) semi-mod P;
-        y = (y * x120) semi-mod P;
-        c = c + pack120(data + i);
-        y = y + pack120(data + i + 1);
+        { // *= x^120 semi-mod P
+            __m128i k = _mm_setr_epi32(
+                0, 0x4b334000 /* x^120+64-1 mod P */,
+                0, 0x96d30000 /* x^120-1    mod P */);
+            c = xor_clmul(c, k);
+            y = xor_clmul(y, k);
+        }
+        { // +=
+            c = _mm_xor_si128(c, pack120(data + i));
+            y = _mm_xor_si128(y, pack120(data + i + 1));
+        }
     }
-    c = (c * x14) semi-mod P;
-    y = (y * x14) semi-mod P;
-    crcs[0] = c mod P;
-    crcs[1] = y mod P;
-}
-
-polynomial pack120(const polynomial* data) {
-    return pack60(data) * x64 + pack60(data + 12) * x4;
-}
-
-polynomial pack60(const polynomial* data) {
-    return data[ 0] * x50 +
-           data[ 2] * x40 +
-           data[ 4] * x30 +
-           data[ 6] * x20 +
-           data[ 8] * x10 +
-           data[10];
+    { // *= x^14 semi-mod P
+        __m128i k = _mm_setr_epi32(
+            0, 0x14980000 /* x^14+64-1 mod P */,
+            0, 0x00040000 /* x^14-1    mod P */);
+        c = xor_clmul(c, k);
+        y = xor_clmul(y, k);
+    }
+    { // mod P
+        __m128i k = _mm_setr_epi32( /* x^128-1 div P */
+            0x14980559, 0x4c9bb5d5,
+            0x80040000, 0x5e405011);
+        c = _mm_xor_si128(_mm_srli_si128(xor_clmul(c, k), 8),
+                          _mm_clmulepi64_si128(c, k, 0x01));
+        y = _mm_xor_si128(_mm_srli_si128(xor_clmul(y, k), 8),
+                          _mm_clmulepi64_si128(y, k, 0x01));
+        __m128i P = _mm_cvtsi32_si128(0x46001 /* P */);
+        c = _mm_clmulepi64_si128(c, P, 0x00);
+        y = _mm_clmulepi64_si128(y, P, 0x00);
+    }
+    crcs[0] = _mm_cvtsi128_si32(c);
+    crcs[1] = _mm_cvtsi128_si32(y);
 }
