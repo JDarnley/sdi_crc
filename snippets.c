070c7aac7581534f89c86f358809aeb0f6eaddf0
James Darnley <jdarnley@obe.tv>
2023-11-02 17:18:01 +0100

    Optimising SDI CRC: look up table

--- snippets.c
+++ snippets.c
@@ -1,13 +1,23 @@
+uint16_t table[1u << 10];
+
+void make_table() {
+    for (uint32_t idx = 0; idx < (1u << 10); idx++) {
+        uint32_t c = idx;
+        for (int k = 0; k < 10; k++) {
+            c = c & 1 ? (c >> 1) ^ 0x23000 : c >> 1;
+        }
+        table[idx] = (c >> 2);
+    }
+}
+
 void crc_sdi(uint32_t* crcs, const uint16_t* data, size_t n) {
     uint32_t c = crcs[0];
     uint32_t y = crcs[1];
     for (size_t i = 0; i < n; i += 2) {
         c ^= data[i];
         y ^= data[i+1];
-        for (int k = 0; k < 10; k++) {
-            c = c & 1 ? (c >> 1) ^ 0x23000 : c >> 1;
-            y = y & 1 ? (y >> 1) ^ 0x23000 : y >> 1;
-        }
+        c = (table[c & 0x3ff] << 2) ^ (c >> 10);
+        y = (table[y & 0x3ff] << 2) ^ (y >> 10);
     }
     crcs[0] = c;
     crcs[1] = y;
